4a765a469e56dd7174663c7bab1b5c1178ec7151
Javier <jsilveira@gmail.com>
2017-04-27 16:39:16 -0300

    First version of Text Explorer

---
+++ text-explorer-ctrl.js
@@ -0,0 +1,118 @@
+angular.module('pruebas', []);
+
+function escapeRegExp(string) {
+  return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+const worker = new Worker('text-processing-worker.js');
+
+function TextExplorerCtrl($scope, $sce, $timeout) {
+  // Para poder usar esas funciones en el template de angular
+  $scope.Math = Math;
+  $scope.JSON = JSON;
+  $scope._ = _;
+
+
+  $scope.lastSearchTime = 0;
+  $scope.radio = 30;
+  $scope.forceRegexp = true;
+
+  // Cuantoss resultados se muestran a la vez
+  $scope.shownRows = 50;
+
+  $scope.denuncias = [];
+  $scope.topMatches = [];
+  $scope.partial = true
+
+  let re = new RegExp();
+  $scope.lastRe = re;
+
+  let filterChanged = 0;
+  let queryRegexp = "";
+
+  // Esto crea un watcher para que cada vez que cambia el filtro, actualizo la regular expression
+  // Esto es para no construir el bojeto RegExp en cada denuncia.
+  $scope.$watch("filtroTexto", function(nuevoValor) {
+    if (nuevoValor) {
+      if ($scope.forceRegexp) {
+        try {
+          re = new RegExp(nuevoValor, "ig");
+          $scope.invalidRegexp = false;
+          filterChanged = new Date().valueOf();
+          queryRegexp = nuevoValor;
+          $scope.filtrarDenuncias();
+        } catch (err) {
+          $scope.invalidRegexp = true;
+        }
+      } else {
+        const regexText = escapeRegExp(nuevoValor);
+        re = new RegExp(regexText, "ig");
+        queryRegexp = regexText;
+        $scope.filtrarDenuncias();
+      }
+
+    }
+    else {
+      $scope.invalidRegexp = false;
+      filterChanged = new Date().valueOf();
+      queryRegexp = ""
+      re = new RegExp();
+      $scope.filtrarDenuncias();
+    }
+  });
+
+  let start = new Date();
+  $scope.filtrarDenuncias = function() {
+    start = new Date();
+    worker.postMessage({action: 'search', query: queryRegexp}); // Send data to our worker.
+  };
+
+  worker.addEventListener('message', function(e) {
+    $scope.$apply(function() {
+      const msg = e.data;
+      switch (msg.action) {
+        case "search-result":
+          const res = msg.res;
+          $scope.filtradas = res.filtradas;
+          $scope.filtradasLength = res.filtradasLength;
+          $scope.partial = res.partial;
+          $scope.lastSearchTime = res.lastSearchTime;
+          // if(!res.partial) {
+          $scope.topMatches = res.topMatches;
+          // }
+          $scope.lastRe = re;
+          $scope.realLastTime = new Date() - start;
+          break;
+        case "status":
+          $scope.status = msg.message;
+          break;
+        default:
+          console.error("Unknown message", msg);
+      }
+      console.log('Worker said: ', msg);
+    });
+  }, false);
+
+  // Esto es simplemente para highlightear. Uso la misma regex de búsqueda para wrapear lo que matchea
+  // por un span con una clase css que lugo pinto de amarillo
+  $scope.resaltarResultado = function(texto) {
+    if($scope.filtroTexto){
+      // Si hay filtro, creo una regular expresion con el texto escapeado, por los caracteres especiales
+      $scope.lastRe.lastIndex = 0;
+      const m = $scope.lastRe.exec(texto);
+      const from = Math.max(m.index - parseInt($scope.radio), 0);
+      const to = m.index + parseInt($scope.radio) + m[0].length;
+      texto = texto.slice(from, to)+"...";
+      if(from > 0){
+        texto = "..."+texto;
+      }
+      if(to < texto.length - 1){
+        texto = texto+"...";
+      }
+
+      texto = texto.replace(re, "<span class='resaltado'>$&</span>")
+    }
+
+    return  $sce.trustAsHtml(texto);
+  };
+}
---
+++ text-processing-worker.js
@@ -0,0 +1,200 @@
+importScripts('underscore-min.js');
+
+const stopWordsList = [
+  "de", "del", "con",
+  "y", "ya", "o", "para", "con", "por", "sin",
+  "en", "al", "a",
+  "el", "lo", "los", "le", "les", "la", "las", "yo",
+  "no", "ni",
+  "es", "estan", "se", "he", "son", "esta", "algo", "otra",
+  "tiene", "tengo", "hay", "pueden", "hace", "ha", "era", "da", "saben", "tienen", "puedo",
+  "uno", "unos", "muy", "una", "un", "como", "mi", "desde",
+  "me", "nos", "te",
+  "que", "q", "cuando", "hasta",
+  "mas", "pero", "si"
+];
+
+let stopWords = {};
+_.each(stopWordsList, function(word){
+  stopWords[word] = true;
+});
+
+const cleanupRegexes = [
+  [new RegExp("[àáâãäå]", 'g'), "a"],
+  [new RegExp("æ", 'g'), "ae"],
+  [new RegExp("ç", 'g'), "c"],
+  [new RegExp("[èéêë]", 'g'), "e"],
+  [new RegExp("[ìíîï]", 'g'), "i"],
+  [new RegExp("ñ", 'g'), "n"],
+  [new RegExp("[òóôõö]", 'g'), "o"],
+  [new RegExp("œ", 'g'), "oe"],
+  [new RegExp("[ùúûü]", 'g'), "u"],
+  [new RegExp("[ýÿ]", 'g'), "y"]
+]
+
+function removeDiacriticsCasero(s) {
+  if (s) {
+    s = s.toLowerCase();
+    _.each(cleanupRegexes, ([regex, replace]) => s = s.replace(regex, replace))
+  }
+  return s;
+}
+
+function removeStopWords(cleanSearch) {
+  let words = cleanSearch.split(" ")
+  let res = _.filter(words, w => !stopWords[w]);
+  return res.join(" ")
+}
+
+function getJSON(url, sucessCbk, errCbk = ()=>console.error(err)) {
+  let request = new XMLHttpRequest();
+  request.open('GET', url, true);
+
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      sucessCbk(JSON.parse(request.responseText));
+    } else {
+      errCbk("Error " + request.status)
+    }
+  };
+
+  request.onerror = function (err) {
+    errCbk(err)
+  };
+
+  request.send();
+}
+
+function log(msg) {
+  self.postMessage({action: 'status', message: msg})
+}
+
+self.addEventListener('message', function (msg) {
+  switch (msg.data.action) {
+    case 'search':
+      search(new RegExp(msg.data.query, "ig"));
+      break;
+    default:
+      console.error("Unknown message action", msg.data);
+  }
+}, false);
+
+let stringTest = [];
+let items = [];
+let filtradas = [];
+let topMatches = {};
+let finalTopMatches = {};
+let partial = false;
+
+let searching = false;
+let lastSearchTime = 0;
+let progressSent = false;
+
+let re = new RegExp();
+
+function sendProgress(query){
+  self.postMessage({action: 'search-result', res: {
+    filtradas: filtradas.slice(0, 200000),
+    filtradasLength: filtradas.length,
+    partial,
+    lastSearchTime,
+    topMatches
+  }});
+  console.log("Progress con "+query)
+}
+
+let nextTick = 0;
+function search(regex) {
+  if(items.length === 0) return;
+  re = regex;
+
+  searching = true;
+
+  progressSent = false;
+  topMatches = {};
+  filtradas = [];
+
+  let START = 0;
+
+  let startTime = new Date();
+  let lastPause = new Date();
+
+  function resume(START, query) {
+    for (var i = START; i < items.length; i++) {
+      let den = items[i];
+      let m = (den || "").toString().match(re);
+      if (m) {
+        topMatches[m[0]] = (topMatches[m[0]] || 0) + 1;
+        filtradas.push(den);
+      }
+
+      if (new Date() - lastPause > 30) {
+        lastPause = new Date();
+        START = i;
+
+        log(`Searching ${(START/items.length*100).toFixed(0)}%`);
+        // partial = true;
+        // finalTopMatches = _.sortBy(_.pairs(JSON.parse(JSON.stringify(topMatches))), "1").reverse();
+        // sendProgress(q);
+        break;
+      }
+
+      if (new Date() - startTime > 35 && !progressSent) {
+        partial = true;
+        // topMatches = _.sortBy(_.pairs(topMatches), "1").reverse();
+        sendProgress(query);
+        progressSent = true;
+      }
+    }
+
+    if(i < items.length){
+      nextTick = setTimeout(() => resume(START, query), 0);
+    } else {
+      topMatches = _.sortBy(_.pairs(topMatches), "1").reverse();
+      partial = false;
+      searching = false;
+      lastSearchTime = new Date().valueOf() - startTime;
+      log("");
+
+      sendProgress(query);
+    }
+  }
+
+  clearTimeout(nextTick);
+  resume(START, regex.toString());
+}
+
+log("Loading huge file...");
+
+let input = "input/sample.json";
+
+getJSON(input, function (data) {
+  items = [];
+  log("Preprocessing " + data.length + " strings...");
+  let lastStatus = new Date();
+  _.each(data, function (search, index) {
+    // let [userId, search] = line.split("\t")
+    try {
+      let cleanSearch = removeDiacriticsCasero(search || "").trim()
+      // cleanSearch = removeStopWords(cleanSearch)
+      items.push(cleanSearch)
+    } catch(err){
+      console.error(err, search)
+    }
+
+    if(new Date() - lastStatus > 400){
+      log("Processing " + data.length + " strings... ("+(100*index/data.length).toFixed(1)+"%)");
+      lastStatus = new Date();
+    }
+  });
+
+  log(``);
+
+  filtradas = items
+  stringTest = items.slice(0, 10000);
+
+  sendProgress("");
+
+}, function(e){
+  log(`Error loading '${input}': ${e.toString()}`)
+});
