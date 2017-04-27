19af514ddc97e47114faa3ce9100b8c0db37be2c
Javier <jsilveira@gmail.com>
2017-04-27 20:13:11 -0300

    Add english stopwords

--- text-processing-worker.js
+++ text-processing-worker.js
@@ -15,6 +15,8 @@ const stopWordsList = [
   "mas", "pero", "si"
 ];
 
+const stopWordsEnglish = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours	ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
+
 let stopWords = {};
 _.each(stopWordsList, function(word){
   stopWords[word] = true;
@@ -31,8 +33,8 @@ const cleanupRegexes = [
   [new RegExp("œ", 'g'), "oe"],
   [new RegExp("[ùúûü]", 'g'), "u"],
   [new RegExp("[ýÿ]", 'g'), "y"]
-]
 
+]
 function removeDiacriticsCasero(s) {
   if (s) {
     s = s.toLowerCase();
@@ -186,7 +188,7 @@ function parseDataObject(data){
     if(_.isObject(search)){
       search = _.map(_.values(search), v => v.toString()).join(" ||| ")
     }
-    // let [userId, search] = line.split("\t")
+
     try {
       let cleanSearch = removeDiacriticsCasero(search || "").trim()
       // cleanSearch = removeStopWords(cleanSearch)
